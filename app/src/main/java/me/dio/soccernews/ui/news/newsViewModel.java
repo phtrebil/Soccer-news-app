870180ed2fa8cdd8f19fdcae5fe626c10f239f4b
PEdro <phptrebil@gmail.com>
2022-07-10 20:17:43 -0300

    implementando os conteúdos da APi desenvolvida no pages do GitHub, utilizando as implementações Picasso e Retrofit

--- app/src/main/java/me/dio/soccernews/ui/news/newsViewModel.java
+++ app/src/main/java/me/dio/soccernews/ui/news/newsViewModel.java
@@ -7,24 +7,48 @@ import androidx.lifecycle.ViewModel;
 import java.util.ArrayList;
 import java.util.List;
 
+import me.dio.soccernews.data.remote.SoccerNewsApi;
 import me.dio.soccernews.domain.News;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class newsViewModel extends ViewModel {
 
-    private final MutableLiveData<List<News>> news;
+    private final MutableLiveData<List<News>> news = new MutableLiveData<>();
+    private final SoccerNewsApi api;
 
     public newsViewModel() {
-        this.news = new MutableLiveData<>();
-        List<News> news = new ArrayList<>();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://phtrebil.github.io/Soccer-news-api/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
 
-        //remover mock de notícias
-        news.add(new News("Ferroviária Desfalque Importante", "kdflnsçlfgtjsdopikfgjsdofjsadofjigsdo fjsdoijtfsdoijft sadoipt soaifgjt sopadjifg sdopijfgsadoifgjsdojg spoigjsd"));
-        news.add(new News("Flamengo bate Corinthians", "s gjfsdlgsd oifgtjsdpogsoighsd u ghsdpoighsdiufgh sdpo ghsdfpoighfsdpoiug hsdfipougsdfpoiuhg sdifpg hfsdoiupg oifpsduhg"));
-        news.add(new News("Palmeiras perde de novo", "o sdjgçosdiuh glisduh gsdah gsdoiuh gsdiuhg siudh goisdufhg oiufsdh goiufsdh goiufsdh gioufsdh giuofsdh goiud giuiuh "));
-        this.news.setValue(news);
+        api = retrofit.create(SoccerNewsApi.class);
+        findNews();
+    }
+
+    private void findNews() {
+        api.getNews().enqueue(new Callback<List<News>>() {
+            @Override
+            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+                if(response.isSuccessful()){
+                    news.setValue(response.body());
+                }else{
+                    //TODO pensar estratégia de tratamento de erros
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<News>> call, Throwable t) {
+                //TODO pensar estratégia de tratamento de erros
+            }
+        });
     }
 
     public LiveData<List<News>> getNews() {
-        return news;
+        return this.news;
     }
 }
